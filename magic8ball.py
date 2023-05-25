021905e148ab35c9c5e5c99270c104bfe865ca10
DS98 <102718187+DS98@users.noreply.github.com>
2023-05-25 15:41:48 -0500

    Update magic8ball.py

--- magic8ball.py
+++ magic8ball.py
@@ -1,1 +1,17 @@
+import random
+Question = input('What would you like to know?: ')
+possible_answers = ["Yes - definitely!",
+"It is decidedly so!",
+"Without a doubt!",
+"Reply hazy, try again.",
+"Ask again later.",
+"Better not tell you now.",
+"My sources say no.",
+"Outlook not so good.",
+"Very doubtful."]
 
+random_index = random.randint(0, len(possible_answers) -1)
+
+answer = possible_answers[random_index]
+
+print(answer)
